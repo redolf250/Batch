7bd3f633c8615c015dfd5e42b08ac1772ad461cf
redolf250 <redolfkendrick@gmail.com>
2022-08-11 16:49:40 +0000

    Created a new session to handle saving of object.

--- src/main/java/com/redolf/application/batch/backend/service/ParameterService.java
+++ src/main/java/com/redolf/application/batch/backend/service/ParameterService.java
@@ -1,5 +1,47 @@
 package com.redolf.application.batch.backend.service;
 
-public class ParameterService {
+import com.redolf.application.batch.backend.models.*;
+import com.redolf.application.batch.frontend.utils.HibernateUtil;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public  class ParameterService {
+
+    public static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactoryForParameters() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/batch?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "0552588647");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(Parameters.class)
+                        .addAnnotatedClass(Summary.class)
+                        .addAnnotatedClass(Status.class)
+                        .addAnnotatedClass(Schedule.class)
+                        .addAnnotatedClass(Types.class)
+                        .addAnnotatedClass(BatchCategory.class);
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 
 }
